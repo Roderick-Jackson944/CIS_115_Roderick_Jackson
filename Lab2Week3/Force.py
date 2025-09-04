61e0318a6558312fb99a0c39bd46151cc900fb39
SANDHILLS\LH218-18 <jad.talbert@gmail.com>
2025-09-04 10:42:46 -0400

    Week 3

---
+++ Lab2Week3/Force.py
@@ -0,0 +1,4 @@
+num1 = 10
+num2 = 9.81
+product = float(num1) * float(num2)
+print('the Force of {0} Kg going at {1} m/s^2 is {2} Kg of Force'.format(num1, num2, product))
---
+++ Lab2Week3/Velocity.py
@@ -0,0 +1,4 @@
+num1 = 400
+num2 = 4
+diff = float(num1) / float(num2)
+print('the velocity if travaled {0} Miles in {1} hours is {2} miles per hour'.format(num1, num2, diff))
