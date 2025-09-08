fc24bc69a884a56bdc9bb669e7670ab3da62a288
SANDHILLS\LH218-18 <jad.talbert@gmail.com>
2025-09-08 09:03:21 -0400

    did this yesterday

---
+++ Math lab/Distance.py
@@ -0,0 +1,4 @@
+num1 = 80
+num2 = 2
+product = float(num1) * float(num2)
+print('the distance travaled at {0} MPH for {1} hours is {2} miles'.format(num1, num2, product))
---
+++ Math lab/Volt.py
@@ -0,0 +1,4 @@
+num1 = 10
+num2 = 2
+product = float(num1) * float(num2)
+print('the voltage of {0} Amps and {1} Ohms is {2} Volts'.format(num1, num2, product))
