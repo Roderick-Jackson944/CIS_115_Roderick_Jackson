06d3761a72c95d71db4247b7b2821e6e0c47060b
SANDHILLS\LH218-18 <jad.talbert@gmail.com>
2025-09-15 09:28:37 -0400

    takeing a bit

---
+++ Week5lab/enterGrades.py
@@ -0,0 +1,20 @@
+num1 = input('enter grade in numerical form:')
+num2 = input('enter grade in numerical form:')
+num3 = input('enter grade in numerical form:')
+num4 = input('enter grade in numerical form:')
+num5 = input('enter grade in numerical form:')
+num6 = input('enter grade in numerical form:')
+num7 = input('enter grade in numerical form:')
+num8 = input('enter grade in numerical form:')
+num9 = input('enter grade in numerical form:')
+num10 = input('enter grade in numerical form:')
+if (num1 >= 90):
+    print('Grade is an A')
+elif (90 > num1 >= 80):
+    print('Grade is an B')
+elif (80> num1 >= 70):
+    print('Grade is an C')
+elif (70> num1 >= 60):
+    print('Grade is an D')
+else:
+    print('Grade is an F')
