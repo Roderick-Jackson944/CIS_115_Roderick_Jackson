22c16af8d5806580323b5c6b635461418835f187
SANDHILLS\lh178-07 <jad.talbert@gmail.com>
2025-10-09 09:19:12 -0400

    done

---
+++ Week8/Week8lab4.py
@@ -0,0 +1,8 @@
+def surch():
+    months= ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'Augest', 'September', 'October', 'November', 'December','january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'augest', 'september', 'october', 'november', 'december']
+    surching = input('what month are you looking for?:')
+    if surching in months:
+        print(f'we found {surching} in the list, Search successful!')
+    else:
+        print(f'cannot find {surching} in the months list')
+surch()
