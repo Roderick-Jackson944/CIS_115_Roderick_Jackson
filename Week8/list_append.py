ce91eb863d1a67c2c26332b355bad45884f360d5
SANDHILLS\LH218-18 <jad.talbert@gmail.com>
2025-10-22 09:15:32 -0400

    Yippe

--- Week8/list_append.py
+++ Week8/list_append.py
@@ -6,9 +6,10 @@ while done:#loop
     yes = input('Would you like to enter another value to append to the list?(y or n):')
     if yes == 'n':
         print(f'{list}')
-        done = False#ends loop
+        done = False #ends loop
     elif yes == 'y':
         list.append(input('Insert any value here to input into list again:'))
         yes = input('Would you like to enter another value to append to the list?(y or n):')
     else:
         print(f'error, here is recorded list {list}')
+        done = False #ends loop
