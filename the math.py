975d76dce5bfd8d96531f74cc7a82ec858085291
SANDHILLS\LH218-18 <jad.talbert@gmail.com>
2025-11-19 09:13:51 -0500

    change the thing out, every one needs their own

---
+++ the math.py
@@ -0,0 +1,22 @@
+litst_totalnum = []
+produtct_select = input('input the item number: ')
+if produtct_select == 1:
+    print('Item; Dsb Drive (128gb), selected: ')
+    numberwhanted = int(input('Amount needed: '))
+    litst_totalnum.append( 12 * numberwhanted )
+elif produtct_select == 2:
+    print('Item; Mac book Pro (15 in), selected: ')
+    numberwhanted = int(input('Amount needed: '))
+    litst_totalnum.append( 2900 * numberwhanted )
+elif produtct_select == 3:
+    print('Item; Arduino 1010 (with blue tooth), selected: ')
+    numberwhanted = int(input('Amount needed: '))
+    litst_totalnum.append( 48 * numberwhanted )
+elif produtct_select == 4:
+    print('Item; Ring Camera (wireless), selected: ')
+    numberwhanted = int(input('Amount needed: '))
+    litst_totalnum.append( 156 * numberwhanted )
+elif produtct_select == 5:
+    print('Item; Smart TV (TCL 70 inch), selected: ')
+    numberwhanted = int(input('Amount needed: '))
+    litst_totalnum.append( 359 * numberwhanted )
