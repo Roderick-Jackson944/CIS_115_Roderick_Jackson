6d22a924d668fa48cfc94d1f4fc4b04277529203
SANDHILLS\lh178-08 <jad.talbert@gmail.com>
2025-11-20 09:08:06 -0500

    lists done

--- the math.py
+++ the math.py
@@ -1,22 +1,26 @@
-litst_totalnum = []
+Usb = []
+macbookpro = []
+arduino = []
+ringcam = []
+smorttv = []
 produtct_select = input('input the item number: ')
 if produtct_select == 1:
-    print('Item; Dsb Drive (128gb), selected: ')
+    print('Item; Usb Drive (128gb), selected: ')
     numberwhanted = int(input('Amount needed: '))
-    litst_totalnum.append( 12 * numberwhanted )
+    Usb.append( 12 * numberwhanted )
 elif produtct_select == 2:
     print('Item; Mac book Pro (15 in), selected: ')
     numberwhanted = int(input('Amount needed: '))
-    litst_totalnum.append( 2900 * numberwhanted )
+    macbookpro.append( 2900 * numberwhanted )
 elif produtct_select == 3:
     print('Item; Arduino 1010 (with blue tooth), selected: ')
     numberwhanted = int(input('Amount needed: '))
-    litst_totalnum.append( 48 * numberwhanted )
+    arduino.append( 48 * numberwhanted )
 elif produtct_select == 4:
     print('Item; Ring Camera (wireless), selected: ')
     numberwhanted = int(input('Amount needed: '))
-    litst_totalnum.append( 156 * numberwhanted )
+    ringcam.append( 156 * numberwhanted )
 elif produtct_select == 5:
     print('Item; Smart TV (TCL 70 inch), selected: ')
     numberwhanted = int(input('Amount needed: '))
-    litst_totalnum.append( 359 * numberwhanted )
+    smorttv.append( 359 * numberwhanted )
